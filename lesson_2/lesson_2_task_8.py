d39310c9c5cbebc60d385c0c5a135e49bd619da8
evgig <evg.ignatova87@gmail.com>
2023-07-04 10:29:27 +0300

    lesson_2

--- lesson_2/lesson_2_task_8.py
+++ lesson_2/lesson_2_task_8.py
@@ -1,7 +1,2 @@
-# Задание 8. Range
-
-# Изучите статью: https://pythonchik.ru/osnovy/python-range.
-# Создайте список [ 18, 14, 10, 6, 2 ] с помощью функции range() и выведите его на экран.
-
 for i in range(18, 0, -4):
     print(i, end = ' ')
